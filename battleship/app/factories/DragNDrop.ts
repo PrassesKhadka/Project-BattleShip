106e6bd2c59fda4cda445f756758d4b0fce1988b
Prasses Khadka <khadkaprasses@gmail.com>
2023-10-27 03:25:47 -0400

    DragNDrop: placeValid function test cases passed

--- battleship/app/factories/DragNDrop.ts
+++ battleship/app/factories/DragNDrop.ts
@@ -1,10 +1,17 @@
+import { Ilocation } from "./Gameboard";
+import { IreturnPlayer } from "./Player";
 import Ship, { IreturnShip } from "./Ship";
 
 export interface IreturnShipDrag {
 	ships: IreturnShip[][];
+	placeValid: (
+		ship: IreturnShip,
+		offset: number,
+		onDragLocation: Ilocation
+	) => boolean;
 }
 
-export default function ShipDrag(): IreturnShipDrag {
+export default function ShipDrag(player: IreturnPlayer): IreturnShipDrag {
 	let length = 2;
 	const ships: IreturnShip[][] = [];
 
@@ -20,5 +27,40 @@ export default function ShipDrag(): IreturnShipDrag {
 		length++;
 	}
 
-	return { ships };
+	// checking if the place on board is valid for the dragged ship to be placed
+	// if valid place and return true else return false
+	function placeValid(
+		ship: IreturnShip,
+		offset: number,
+		onDragLocation: Ilocation
+	): boolean {
+		// onDragLocation--->basically onDragLeaveLocationOnBoard
+		const startingPosition = position(ship, offset, onDragLocation);
+		const valid = player.getGameBoard().placeShip(ship, startingPosition);
+		console.log(valid);
+		return valid;
+	}
+
+	//returns the startingPosition of the ship on the board
+	function position(
+		shipDragged: IreturnShip,
+		offset: number,
+		onDragLocationOnBoard: Ilocation
+	): Ilocation {
+		let startingPosition: Ilocation;
+		if (shipDragged.getIsHorizontal() === true) {
+			startingPosition = {
+				x: onDragLocationOnBoard.x,
+				y: onDragLocationOnBoard.y - offset,
+			};
+		} else {
+			startingPosition = {
+				x: onDragLocationOnBoard.x - offset,
+				y: onDragLocationOnBoard.y,
+			};
+		}
+		console.log(startingPosition);
+		return startingPosition;
+	}
+	return { ships, placeValid };
 }
--- battleship/app/factories/__tests__/DragNDrop.test.ts
+++ battleship/app/factories/__tests__/DragNDrop.test.ts
@@ -1,12 +1,37 @@
-import ShipDrag from "../DragNDrop";
+import ShipDrag, { IreturnShipDrag } from "../DragNDrop";
+import Player, { IreturnPlayer } from "../Player";
 import { IreturnShip } from "../Ship";
+import { Ilocation } from "../Gameboard";
 
 describe("DragNDrop", () => {
+	let arrayShips: IreturnShipDrag;
+	const player: IreturnPlayer = Player("Prasses");
+
+	beforeEach(() => {
+		arrayShips = ShipDrag(player);
+	});
+
 	it("Creates ships", () => {
-		const ships = ShipDrag();
-		ships.ships.forEach((subArray) => {
+		arrayShips.ships.forEach((subArray) => {
 			expect(subArray.every((ship) => typeof ship === "object")).toBe(true);
 		});
-		console.log(ships);
+		// console.log(arrayShips);
+	});
+	it("placeShips on the gameboard", () => {
+		// ship that is being dragged
+		// horizontal placing
+		const shipDragged1 = arrayShips.ships[0][0];
+		// vertical placing
+		arrayShips.ships[1][0].isToggleDirection();
+		const shipDragged2 = arrayShips.ships[1][0];
+		// const onDragLocationOnShip = 1;
+		// therefore the offset is onDragLocationOnShip
+		const offset = 1;
+		const result1 = arrayShips.placeValid(shipDragged1, offset, { x: 0, y: 1 });
+		const result2 = arrayShips.placeValid(shipDragged2, offset, { x: 8, y: 4 });
+		console.log(result1);
+		expect(result1).toBe(true);
+		expect(result2).toBe(true);
+		console.log(player.getGameBoard().board);
 	});
 });
