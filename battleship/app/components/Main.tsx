2ee863d592a36f33eabf7c7ae97b024834345ea9
Prasses <khadkaprasses@gmail.com>
2024-02-05 11:40:06 -0500

    Removed footer

--- battleship/app/components/Main.tsx
+++ battleship/app/components/Main.tsx
@@ -7,54 +7,52 @@ import { IreturnGameBoard, TboardData } from "../factories/Gameboard";
 import Footer from "./Footer";
 
 const Main = () => {
-	const [start, setStart] = useState<boolean>(false);
-	let board1: TboardData[][] = [];
-	let board2: TboardData[][] = [];
+  const [start, setStart] = useState<boolean>(false);
+  let board1: TboardData[][] = [];
+  let board2: TboardData[][] = [];
 
-	// For player1
-	const player1 = useRef(Player("Prasses"));
-	const player1Gameboard = player1.current.getGameBoard();
-	board1 = player1Gameboard.board;
-	// player1.randomlyAddShip();
-	// setting player1's turn first
-	// player1.current.toggleIsTurn();
+  // For player1
+  const player1 = useRef(Player("Prasses"));
+  const player1Gameboard = player1.current.getGameBoard();
+  board1 = player1Gameboard.board;
+  // player1.randomlyAddShip();
+  // setting player1's turn first
+  // player1.current.toggleIsTurn();
 
-	// For player2
-	const player2 = Player("Computer");
-	const player2Gameboard = player2.getGameBoard();
-	board2 = player2Gameboard.board;
-	player2.randomlyAddShip();
+  // For player2
+  const player2 = Player("Computer");
+  const player2Gameboard = player2.getGameBoard();
+  board2 = player2Gameboard.board;
+  player2.randomlyAddShip();
 
-	return (
-		<div className="relative bg-white w-screen min-h-screen flex flex-col justify-center items-center">
-			{/* {!start ? (
+  return (
+    <div className="relative bg-white w-screen min-h-screen flex flex-col justify-center items-center">
+      {/* {!start ? (
 				<div className="fixed bg-red-300 left-0 w-[30%] min-h-screen overflow-y-scroll rounded-sm bg-gradient-to-r from-rose-400 to-red-500">
 					<Sidebar />
 				</div>
 			) : null} */}
 
-			{!start ? (
-				<Start player={player1.current} board={board1} setStart={setStart} />
-			) : (
-				<div className="flex flex-wrap justify-center items-center gap-12">
-					<Board
-						board={board1}
-						player={player1.current}
-						opponent={player2}
-						setStart={setStart}
-					/>
-					<Board
-						board={board2}
-						player={player2}
-						opponent={player1.current}
-						setStart={setStart}
-					/>
-				</div>
-			)}
-
-			<Footer/>
-		</div>
-	);
+      {!start ? (
+        <Start player={player1.current} board={board1} setStart={setStart} />
+      ) : (
+        <div className="flex flex-wrap justify-center items-center gap-12">
+          <Board
+            board={board1}
+            player={player1.current}
+            opponent={player2}
+            setStart={setStart}
+          />
+          <Board
+            board={board2}
+            player={player2}
+            opponent={player1.current}
+            setStart={setStart}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Main;
